9f37be47a370a117be9675de399233bf6018f9f5
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2013-10-26 01:49:34 +0100

    initial project

---
+++ VPIRC/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VPIRC")]
+[assembly: AssemblyDescription("Provides IRC bridging between a Virtual Paradise world and an IRC channel")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Roy Curtis")]
+[assembly: AssemblyProduct("VPIRC")]
+[assembly: AssemblyCopyright("WTFPL2")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d9088e6f-b7c1-41e1-a111-80fe39304355")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
